4afe427715b1ae46146bf09c151b99ba9e85535e
Anthony Bosek <anthony.bosek@gmail.com>
2023-10-04 06:26:49 -0700

    initial lab setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
 
-function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
-}
+const App = () => {
+  const [hogArr, setHogArr] = useState(hogs);
+
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+};
 
 export default App;
